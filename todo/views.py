44ac480fc5cb6071d1e55f8d1d3d094be5b88641
ronaldhsiao <ronald032001@yahoo.com.tw>
2024-03-21 21:08:31 +0800

    完成代辦事項顯示功能

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 def todo(request):
-    return render(request,"todo/todo.html")
+    # todos = Todo.objects.all()
+    todos = None
+    if request.user.is_authenticated:
+        todos = Todo.objects.filter(user=request.user)
+
+    return render(request,"todo/todo.html",{"todos":todos})
