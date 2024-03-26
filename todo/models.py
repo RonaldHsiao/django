1b4dd64522d8b66f131fd0336d49980d8e20cae7
ronaldhsiao <ronald032001@yahoo.com.tw>
2024-03-26 20:51:07 +0800

    完成修改跟刪除功能

--- todo/models.py
+++ todo/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length = 100)
-    text = models.TextField(blank = True)
-    created = models.DateField(auto_now_add = True)
-    date_completed = models.DateTimeField(blank = True, null = True)
-    important = models.BooleanField(default = False)
-
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+    date_completed = models.DateTimeField(blank=True, null=True)
+    important = models.BooleanField(default=False)
+    # todo_id<=>user_id
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.id}-{self.created}-{self.title}-{self.user}"
+        return f"{self.id}-[{self.created}] {self.title}-({self.user})"
--- todo/views.py
+++ todo/views.py
@@ -1,36 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 from .forms import TodoForm
+from datetime import datetime
+
 
 def create_todo(request):
+    # GET
     message = ""
     form = TodoForm()
+    # POST
     if request.method == "POST":
         try:
             form = TodoForm(request.POST)
             new_todo = form.save(commit=False)
             new_todo.user = request.user
             new_todo.save()
-            message = "建立事項成功"
+            message = "建立事項成功!"
+
+            return redirect("todolist")
         except Exception as e:
             print(e)
-            message = "建立事項失敗"
-    return render(request,"todo/create-todo.html",{"form":form},{"message":message})
+            message = "建立事項失敗..."
+    return render(request, "todo/create-todo.html", {"form": form, "message": message})
 
 
 # Create your views here.
 def todolist(request):
+    # all,get,filter
     # todos = Todo.objects.all()
     todos = None
     if request.user.is_authenticated:
-        todos = Todo.objects.filter(user=request.user)
+        todos = Todo.objects.filter(user=request.user).order_by("-created")
 
-    return render(request,"todo/todo.html",{"todos":todos})
+    return render(request, "todo/todo.html", {"todos": todos})
+
 
 def view_todo(request, id):
     todo = None
+    message = ""
     try:
-        todo = Todo.objects.get(id = id)
+        todo = Todo.objects.get(id=id)
+        form = TodoForm(instance=todo)
+
+        if request.method == "POST":
+            print(request.POST)
+            if request.POST.get("update"):
+                todo.date_completed = (
+                    datetime.now() if request.POST.get("completed") else None
+                )
+
+                form = TodoForm(request.POST, instance=todo)
+                if form.is_valid():
+                    form.save()
+                    message = "修改成功!"
+            elif request.POST.get("delete"):
+                todo.delete()
+                return redirect("todolist")
+
     except Exception as e:
         print(e)
-    return render(request,"todo/view-todo.html",{"todo":todo})
+        message = "修改或刪除失敗..."
+
+    return render(
+        request, "todo/view-todo.html", {"todo": todo, "form": form, "message": message}
+    )
