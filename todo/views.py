fc965a11e703bbd64983c68c7f37dc7cbb001df4
ronaldhsiao <ronald032001@yahoo.com.tw>
2024-03-26 21:44:37 +0800

    完成刪除確認跟完成事項

--- todo/views.py
+++ todo/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render, redirect
 from .models import Todo
 from .forms import TodoForm
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
+@login_required
+def delete_todo(request,id):
+    try:            
+        todo = Todo.objects.get(id=id)
+        todo.delete()
+    except Exception as e:
+        print(e)
+    return redirect("todolist")
+@login_required
+def completed_todo(request):
+    todos = None
+    completed = True
+    if request.user.is_authenticated:
+        todos = Todo.objects.filter(user=request.user,completed=True).order_by("created")
 
+    return render(request, "todo/todo.html", {"todos": todos,"completed":completed})
+
+@login_required
 def create_todo(request):
     # GET
     message = ""
@@ -30,11 +48,11 @@ def todolist(request):
     # todos = Todo.objects.all()
     todos = None
     if request.user.is_authenticated:
-        todos = Todo.objects.filter(user=request.user).order_by("-created")
+        todos = Todo.objects.filter(user=request.user).order_by("created")
 
     return render(request, "todo/todo.html", {"todos": todos})
 
-
+@login_required
 def view_todo(request, id):
     todo = None
     message = ""
