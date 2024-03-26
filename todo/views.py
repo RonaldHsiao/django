2fc84fe23615536381a38ddb0354c4da67bb4b78
ronaldhsiao <ronald032001@yahoo.com.tw>
2024-03-26 19:44:43 +0800

    完成建立事項表單

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from .models import Todo
+from .forms import TodoForm
+
+def create_todo(request):
+    message = ""
+    form = TodoForm()
+    if request.method == "POST":
+        try:
+            form = TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            message = "建立事項成功"
+        except Exception as e:
+            print(e)
+            message = "建立事項失敗"
+    return render(request,"todo/create-todo.html",{"form":form},{"message":message})
+
 
 # Create your views here.
-def todo(request):
+def todolist(request):
     # todos = Todo.objects.all()
     todos = None
     if request.user.is_authenticated:
